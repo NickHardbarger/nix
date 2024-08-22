5fbde87c8ff06f03db96b45185ecb403e4a3dd54
Nick <nickhardbarger13@gmail.com>
2024-08-22 19:15:26 -0400

    now trying kernel 6.1

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -27,8 +27,9 @@
       name = "envy-sound";
       patch = ./envy-sound.diff;
     } ];
+    kernelPackages = pkgs.linuxPackages_6_1;
     #kernelPackages = pkgs.linuxPackages;
-    kernelPackages = pkgs.linuxPackages_latest;
+    #kernelPackages = pkgs.linuxPackages_latest;
     #kernelPackages = pkgs.linuxPackages_hardened;
     #kernelPackages = pkgs.linuxPackages_zen;
   };
