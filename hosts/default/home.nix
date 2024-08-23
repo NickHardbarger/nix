1596ad9c5c655ad10101352841c137ddb52c9092
Nick <nickhardbarger13@gmail.com>
2024-08-23 04:09:07 -0400

    removed comment

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -68,8 +68,6 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    #"~/.bashrc".source = ./.bashrc;
-    #".config/sway/config".source = ./sway.config;
   };
 
   home.sessionVariables = {
