ae65cec0bc2cec50305a80b5312c61a62a2119c9
NickHardbarger <nickhardbarger13@gmail.com>
2024-10-08 00:37:51 -0400

    emacs/init.el: commented out hook line

--- hosts/t420/emacs/init.el
+++ hosts/t420/emacs/init.el
@@ -16,7 +16,7 @@
 (require 'tree-sitter)
 (require 'tree-sitter-langs)
 (global-tree-sitter-mode)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+;(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
