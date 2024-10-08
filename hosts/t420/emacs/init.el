2a203e56913a07b152a8e29f9d1ae4cbbe2ab024
NickHardbarger <nickhardbarger13@gmail.com>
2024-10-08 00:13:13 -0400

    emacs/init.el: changed theme to gruvbox-dark-medium

--- hosts/t420/emacs/init.el
+++ hosts/t420/emacs/init.el
@@ -22,7 +22,7 @@
 (global-display-line-numbers-mode 1)
 (hl-line-mode 1)
 (blink-cursor-mode 1)
-(load-theme 'gruvbox t)
+(load-theme 'gruvbox-dark-medium t)
 ;; EVIL MODE ;;
 ; Set up package.el to work with MELPA
 (require 'package)
