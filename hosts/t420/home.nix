7836bc8c8f479d0ecc17defaef70760fcbecf9cf
NickHardbarger <nickhardbarger13@gmail.com>
2024-10-16 18:08:48 -0400

    removed sack alias

--- hosts/t420/home.nix
+++ hosts/t420/home.nix
@@ -51,7 +51,6 @@
     "switch-t420" = "sudo nixos-rebuild switch --flake ~/nicksOS#default";
     "switch-lock-t420" = "sudo nixos-rebuild switch --recreate-lock-file --flake ~/nicksOS#default";
     "ngc" = "nix-collect-garbage -d";
-    "sack" = "sudo git add * && sudo git commit -m";
     "qstart" = "qtile start -b wayland";
     "cmatrix" = "cmatrix -C cyan";
     ".." = "cd ..";
