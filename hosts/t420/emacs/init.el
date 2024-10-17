41225472903469ea39fbf3757e3523fd75eb4538
Nick <nickhardbarger13@gmail.com>
2024-10-17 16:30:38 -0400

    removed evil!

--- hosts/t420/emacs/init.el
+++ hosts/t420/emacs/init.el
@@ -28,30 +28,9 @@
 (hl-line-mode 1)
 (blink-cursor-mode 1)
 (load-theme 'gruvbox-dark-hard t)
-;; EVIL MODE ;;
 ; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
-
-; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-; Enable Evil
-;(require 'evil)
-(evil-mode -1) ; refraining from evil... for now
-(custom-set-variables
- ; custom-set-variables was added by Custom.
- ; If you edit it by hand, you could mess it up, so be careful.
- ; Your init file should contain only one such instance.
- ; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
-(custom-set-faces
- ; custom-set-faces was added by Custom.
- ; If you edit it by hand, you could mess it up, so be careful.
- ; Your init file should contain only one such instance.
- ; If there is more than one, they won't work right.
- )
