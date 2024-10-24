9fcf6250a6f9e48c2f7f376847c5532728701449
Nick <nickhardbarger13@gmail.com>
2024-10-23 23:06:14 -0400

    version tweak again

--- hosts/t420/config.nix
+++ hosts/t420/config.nix
@@ -35,7 +35,7 @@
       version = "6.11.5";
       #modDirVersion = "6.11.5";
       src = pkgs.fetchurl {
-        url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
+        url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.11.5.tar.xz";
 	hash = "sha256:01rafnqal2v96dzkabz0irymq4sc9ja00ggyv1xn7yzjnyrqa527";
       };
       configfile = ./linux/.config;
