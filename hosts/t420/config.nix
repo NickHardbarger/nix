d2db0c89f2f51f0780a244ca48fec53ced270e00
Nick <nickhardbarger13@gmail.com>
2024-10-24 00:33:13 -0400

    tweak

--- hosts/t420/config.nix
+++ hosts/t420/config.nix
@@ -42,7 +42,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackagesFor pkgs.linux_zen;
+    kernelPackages = pkgs.linuxPackagesFor pkgs.linuxZennn;
     #kernelPackages = pkgs.linuxPackages;
     #kernelPackages = pkgs.linuxPackages_latest;
     #kernelPackages = pkgs.linuxPackages_hardened;
