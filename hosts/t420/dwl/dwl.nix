c0c3320cee21ade9a13b12bdf6d4ba2a1b03ec37
Nick <nickhardbarger13@gmail.com>
2024-11-04 03:36:37 -0500

    patching dwl

--- hosts/t420/dwl/dwl.nix
+++ hosts/t420/dwl/dwl.nix
@@ -4,7 +4,11 @@
       dwl = super.dwl.overrideAttrs (oldAttrs: rec {
         src = dwl-source;
 	patches = [
+	  ./autostart-0.7.patch
 	  ./bar-0.7.patch
+	  ./client-opacity.patch
+	  ./swallow.patch
+	  #./vanitygaps-0.7.patch
 	];
       });
     })
