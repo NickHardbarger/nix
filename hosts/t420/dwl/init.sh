816298e5e1f6dcd998bcd60363f4e8f8eeddfa89
Nick <nickhardbarger13@gmail.com>
2024-11-04 00:14:21 -0500

    added daemons

--- hosts/t420/dwl/init.sh
+++ hosts/t420/dwl/init.sh
@@ -3,3 +3,7 @@
 # executes notification daemon and sets wallpaper
 exec mako
 exec swaybg -i ~/nil/hosts/t420/wallpapers/stairs.jpg -m fill
+
+# executes foot and emacs daemons
+exec foot --server
+exec emacs --daemon
