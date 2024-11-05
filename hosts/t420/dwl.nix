091c89f28dafa0bf1bfb2659dff1decd34e3cd4b
Nick <nickhardbarger13@gmail.com>
2024-11-05 18:52:38 -0500

    dwlstart script

--- hosts/t420/dwl.nix
+++ hosts/t420/dwl.nix
@@ -4,3 +4,19 @@
 pkgs.writeShellScriptBin "dwlinit" ''
   /home/iglu/dwl/dwl
 ''
+pkgs.writeShellApplication {
+  name = "dwlstart";
+  runtimeInputs = with pkgs; [
+    wayland
+    wayland-scanner
+    wlroots
+    libinput
+    libxkbcommon
+    pixman
+    libdrm
+    fcft
+  ];
+  text = ''
+    /home/iglu/dwl/dwl
+  '';
+}
