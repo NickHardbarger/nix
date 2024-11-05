c46a902a22641efabb82d73fe9001a6702dbe722
Nick <nickhardbarger13@gmail.com>
2024-11-05 11:48:57 -0500

    uncommented opacity and gaps patches

--- hosts/t420/dwl/dwl.nix
+++ hosts/t420/dwl/dwl.nix
@@ -2,13 +2,12 @@
   nixpkgs.overlays = [
     (self: super: {
       dwl = super.dwl.overrideAttrs (oldAttrs: rec {
-        #src = dwl-source;
 	patches = [
 	  ./autostart-0.7.patch
 	  ./bar-0.7.patch
-	  #./client-opacity.patch
+	  ./client-opacity.patch
 	  ./swallow.patch
-	  #./vanitygaps-0.7.patch
+	  ./vanitygaps-0.7.patch
 	];
       });
     })
