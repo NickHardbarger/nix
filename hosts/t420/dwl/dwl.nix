dd78e7037f3c0f4fe8ce521e0bf7c8512e0295ea
Nick <nickhardbarger13@gmail.com>
2024-11-05 05:52:37 -0500

    commented out patches

--- hosts/t420/dwl/dwl.nix
+++ hosts/t420/dwl/dwl.nix
@@ -4,8 +4,8 @@
       dwl = super.dwl.overrideAttrs (oldAttrs: rec {
         src = dwl-source;
 	patches = [
-	  ./client-opacity.patch
-	  ./swallow.patch
+	  #./client-opacity.patch
+	  #./swallow.patch
 	  ./autostart-0.7.patch
 	  ./vanitygaps-0.7.patch
 	  ./bar-0.7.patch
