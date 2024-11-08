777df7943fe3633892fd10a8d142b9940917c512
Nick <nickhardbarger13@gmail.com>
2024-11-08 05:25:28 -0500

    wrapped statement in let/in block

--- hosts/t420/kernel/kernel.nix
+++ hosts/t420/kernel/kernel.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }: {
-  #version = "6.6.59-custom";
+{ pkgs, ... }:
+let
+  version = "6.6.59-custom";
   modDirVersion = "6.6.59";
   src = fetchTarball {
     url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.59.tar.xz";
     sha256 = "0vd76ccd4li4wsg04gc4nai9f4y1nknz967qby0i53y0v046hq93";
   };
   configfile = ./.config;
+in {
   linux = pkgs.linuxManualConfig {
-    inherit modDirVersion src configfile;
+    inherit version modDirVersion src configfile;
     allowImportFromDerivation = true;
   };
 }
