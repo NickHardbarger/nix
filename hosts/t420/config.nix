bfab8edeb65c291b8f7b73bc5940eef63d99846c
Nick <nickhardbarger13@gmail.com>
2024-11-08 16:56:35 -0500

    commented line

--- hosts/t420/config.nix
+++ hosts/t420/config.nix
@@ -40,7 +40,7 @@
       efi.canTouchEfiVariables = true;
     };
     ### KERNEL ###
-    kernelPackages = pkgs.linuxPackagesFor linux;
+    #kernelPackages = pkgs.linuxPackagesFor linux;
     #kernelPackages = pkgs.linuxPackages_zen;
     #kernelPackages = pkgs.linuxManualConfig {
       #version = "6.6.59";
