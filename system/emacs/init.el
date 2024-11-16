23fdc46addff5c447f092e040c33c5fb72546246
Nick <nickhardbarger13@gmail.com>
2024-11-16 01:45:23 -0500

    commented out dashboard items

--- system/emacs/init.el
+++ system/emacs/init.el
@@ -44,8 +44,8 @@
 	dashboard-filter-agenda-entry 'dashboard-no-filter-agenda)
 
   (setq dashboard-items '((recents . 5)
-			  (bookmarks . 5)
-			  (projects . 5)
+;			  (bookmarks . 5)
+;			  (projects . 5)
 			  (agenda . 5)
 			  (registers . 5)))
   
