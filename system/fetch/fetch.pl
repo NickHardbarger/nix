1b240f7d3f84698ffd470cbff81d584f06d7cfb5
Nick <nickhardbarger13@gmail.com>
2024-11-17 20:24:52 -0500

    added module names

--- system/fetch/fetch.pl
+++ system/fetch/fetch.pl
@@ -4,6 +4,9 @@ $name = "Nick";
 
 print "Hello $name!\n";
 #system("uname -a"); # all
+print "hostname: ";
 system("uname -n"); # hostname
+print "os: ";
 system("uname -o"); # OS
+print "kernel: ";
 system("uname -r"); # kernel
