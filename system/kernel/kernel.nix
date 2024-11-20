033f3c0f39e74f3c712a0f94daf7360a2152a1cf
Nick <nickhardbarger13@gmail.com>
2024-11-19 19:52:57 -0500

    added kernel config

--- system/kernel/kernel.nix
+++ system/kernel/kernel.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 let
-  version = "6.6.61-custom";
-  modDirVersion = "6.6.61";
+  version = "6.12-custom";
+  modDirVersion = "6.12";
   src = pkgs.fetchurl {
-    url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.61.tar.xz";
-    hash = "sha256:02vw0jphzqj8cd5apf8wbjp2wahwcnvw6vchv4z1q3qrz56w53s1";
+    url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.12.tar.xz";
+    hash = "sha256:1sr58vsh39hdwk0z27lg14isqwn4g8m4r7a8z2rsyhkfwlmmd8mi";
   };
   configfile = ./.config;
 in
