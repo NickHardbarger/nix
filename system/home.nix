333e09776f129e183c395bf696d821df494e8db8
Nick <nickhardbarger13@gmail.com>
2024-11-19 22:11:02 -0500

    disabled nixpkgs release check

--- system/home.nix
+++ system/home.nix
@@ -101,7 +101,7 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
-
+  home.enableNixpkgsReleaseCheck = false;
   home.packages = with pkgs; [
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
