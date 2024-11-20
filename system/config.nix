8604e441079bddddbb819cba0614aff1ebb8c213
Nick <nickhardbarger13@gmail.com>
2024-11-20 02:01:45 -0500

    enabled nixos ozone wl

--- system/config.nix
+++ system/config.nix
@@ -330,6 +330,7 @@
       };
     };
   };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = with pkgs; [
     ### DMENU ###
     (dmenu.overrideAttrs (oldAttrs: {
