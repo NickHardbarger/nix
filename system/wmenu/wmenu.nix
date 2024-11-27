6e499f69a1eb61cfd5eda8dc9fd4bb0bb150391c
Nick <nickhardbarger13@gmail.com>
2024-11-27 00:20:23 -0500

    fixed wmenu font size

--- system/wmenu/wmenu.nix
+++ system/wmenu/wmenu.nix
@@ -2,5 +2,12 @@
 
 pkgs.writeShellApplication {
   name = "run-wmenu";
-  text = "wmenu-run -f 'Jetbrains Mono 12'";
+  text = "wmenu-run -if 'JetBrainsMonoNF 12' -p '[Wmenu]'";
 }
+# Flags:
+# -N: normal bg color
+# -n: normal fg color
+# -M: prompt bg color
+# -m: prompt fg color
+# -S: selection bg color
+# -s: selection fg color
