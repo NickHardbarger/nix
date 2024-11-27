8420d13f7e99deb30dd9aac8cefede7c322b3cc3
Nick <nickhardbarger13@gmail.com>
2024-11-27 00:39:21 -0500

    added gruvbox colorscheme

--- system/wmenu/wmenu.nix
+++ system/wmenu/wmenu.nix
@@ -2,9 +2,9 @@
 
 pkgs.writeShellApplication {
   name = "run-wmenu";
-  text = "wmenu-run -if 'JetBrainsMonoNF 12' -p '[Wmenu]'";
+  text = "wmenu-run -if 'JetBrainsMonoNF 12' -p '[Wmenu]' -N 282828 -n ebdbb2 -M 458588 -m 282828 -S 458588 -s 282828";
 }
-# Flags:
+# Color flags:
 # -N: normal bg color
 # -n: normal fg color
 # -M: prompt bg color
