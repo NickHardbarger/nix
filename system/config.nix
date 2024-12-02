a4fca8a6711c0bf29ae7ac240493fc5b73c83bc2
Nick <nickhardbarger13@gmail.com>
2024-12-02 07:57:57 -0500

    added thinkfan levels

--- system/config.nix
+++ system/config.nix
@@ -125,7 +125,46 @@
     blueman.enable = true;
     printing.enable = true;
     ollama.enable = false;
-    thinkfan.enable = true;
+    thinkfan = {
+      enable = true;
+      levels = [
+        [
+          0
+          0
+          55
+        ]
+        [
+          1
+          48
+          60
+        ]
+        [
+          2
+          50
+          61
+        ]
+        [
+          3
+          52
+          63
+        ]
+        [
+          6
+          56
+          65
+        ]
+        [
+          7
+          60
+          85
+        ]
+        [
+          "level auto"
+          80
+          32767
+        ]
+      ];
+    };
     pipewire = {
       enable = true;
       alsa = {
