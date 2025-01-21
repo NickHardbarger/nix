ad778f06e88cb0f25fe89ccf8a1874329dc886a0
NickHardbarger <nickhardbarger13@gmail.com>
2025-01-21 18:50:23 -0500

    modularized locale config

--- system/config.nix
+++ system/config.nix
@@ -22,6 +22,7 @@
     ./firewall.nix
     ./bluetooth.nix
     ./audio.nix
+    ./locale.nix
   ];
   networking = {
     hostName = "t420";
@@ -32,21 +33,6 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-  };
-  time.timeZone = "America/New_York";
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
   };
   services = {
     scx = {
