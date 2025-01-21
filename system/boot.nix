c4aa72943605120c8b83b41899feda14841cb1bf
NickHardbarger <nickhardbarger13@gmail.com>
2025-01-21 15:32:22 -0500

    modularized boot config

---
+++ system/boot.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
+{
+  boot = {
+    loader = {
+      grub.enable = false;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 15;
+      };
+      efi.canTouchEfiVariables = true;
+      timeout = 1;
+    };
+    plymouth = {
+      enable = true;
+      theme = "glitch";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "glitch" ];
+        })
+      ];
+    };
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    #kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+  };
+}
--- system/config.nix
+++ system/config.nix
@@ -18,39 +18,8 @@
     ./starship.nix
     ./nh.nix
     ./nix.nix
+    ./boot.nix
   ];
-  boot = {
-    loader = {
-      grub.enable = false;
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 15;
-      };
-      efi.canTouchEfiVariables = true;
-      timeout = 1;
-    };
-    plymouth = {
-      enable = true;
-      theme = "glitch";
-      themePackages = with pkgs; [
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "glitch" ];
-        })
-      ];
-    };
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    #kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
-  };
   networking = {
     hostName = "t420";
     networkmanager.enable = true;
