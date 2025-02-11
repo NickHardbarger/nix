1e4a4f612d167b75f58560b08812833794e06d7f
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-11 18:33:20 -0500

    changed name

--- system/modules/doas.nix
+++ system/modules/doas.nix
@@ -7,7 +7,7 @@
 }:
 {
   security = {
-    sudo.enable = true; # TODO: figure out nil repo ownership issue
+    sudo.enable = true; # TODO: figure out nix repo ownership issue
     doas = {
       enable = false;
       extraRules = [
