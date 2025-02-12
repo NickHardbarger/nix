e50d577db6a8273dc11002935edec053bf868b62
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-12 03:38:07 -0500

    finished prototype

--- system/scripts/gas/main.py
+++ system/scripts/gas/main.py
@@ -1,20 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-#driver.get("https://www.selenium.dev")
+driver.get("https://www.gasbuddy.com/station/30734")
 
 title = driver.title
 
 driver.implicitly_wait(0.5)
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-text_box.send_keys("Selenium")
-submit_button.click()
-
-text = message.text
+text = driver.find_element(By.CLASS_NAME, "text__xl___2MXGo.text__bold___1C6Z_.text__left___1iOw3.FuelTypePriceDisplay-module__price___3iizb").text
 
 driver.quit()
+
+print("The price of gas is:", text)
