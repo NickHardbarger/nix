872a51dcccb0a230380d90e6bf201a23b83fd90d
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-12 09:12:48 -0500

    changed prompt symbol to lambda

--- system/modules/starship.nix
+++ system/modules/starship.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  pkgs,
-  inputs,
-  lib,
-  ...
-}:
+{ ... }:
 {
   programs.starship = {
     enable = true;
@@ -13,8 +7,8 @@
       line_break.disabled = true;
       format = "$all$directory\n$character";
       character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
+        success_symbol = "[󰘧](bold green)";
+        error_symbol = "[󰘧](bold red)";
       };
     };
   };
