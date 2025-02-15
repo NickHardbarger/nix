302dc09441d65e1bb3c6e084bda10e187e2323fb
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-15 05:04:17 -0500

    added symbol to starship prompt
    us
    
    usp

--- system/modules/starship.nix
+++ system/modules/starship.nix
@@ -5,7 +5,7 @@
     settings = {
       add_newline = false;
       line_break.disabled = true;
-      format = "$all$directory\n$character";
+      format = "[╭─](bold blue)$all$directory\n[╰─](bold blue)$character";
       character = {
         success_symbol = "[󰘧](bold green)";
         error_symbol = "[󰘧](bold red)";
