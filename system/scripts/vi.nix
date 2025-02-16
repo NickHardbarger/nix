a53dffdb017cc0d66b0a1d68ff1d740e9dfde35d
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-16 03:47:42 -0500

    fixed script error

--- system/scripts/vi.nix
+++ system/scripts/vi.nix
@@ -2,5 +2,6 @@
 
 pkgs.writeShellApplication {
   name = "vi";
-  text = "exec emacsclient -c -a 'emacs'";
+  text = "exec emacsclient -c -a ''";
+  # "exec emacsclient -c -a 'emacs'" doesn't run emacs daemon
 }
