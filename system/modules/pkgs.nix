48a63963b6e35a268ca28057ca6f4fb3ea8cf648
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-26 03:34:40 -0500

    added line number flag

--- system/modules/pkgs.nix
+++ system/modules/pkgs.nix
@@ -5,7 +5,7 @@
 {
   environment.systemPackages = with pkgs; [
     (pkgs.writeShellScriptBin "run-wmenu" ''
-      ${pkgs.wmenu}/bin/wmenu-run -if 'JetBrainsMonoNF 12' -p '[Wmenu]' -N 282828 -n ebdbb2 -M d65d0e -m 282828 -S d65d0e -s 282828
+      ${pkgs.wmenu}/bin/wmenu-run -if 'JetBrainsMonoNF 12' -l 5 -p '[Wmenu]' -N 282828 -n ebdbb2 -M d65d0e -m 282828 -S d65d0e -s 282828
     '')
     (import ../scripts/start.nix { inherit pkgs; })
     #wbg # wallpaper setter #!!build failure
