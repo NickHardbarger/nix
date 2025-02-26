b75ad49c27fa4dc846cc816060c03f3cd78c48b0
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-26 08:33:38 -0500

    added wmenu flag

--- system/modules/pkgs.nix
+++ system/modules/pkgs.nix
@@ -5,7 +5,7 @@
 {
   environment.systemPackages = with pkgs; [
     (pkgs.writeShellScriptBin "run-wmenu" ''
-      ${pkgs.wmenu}/bin/wmenu-run -if 'JetBrainsMonoNF 12' -p '菜单' -N 282828 -n fe8019 -M 83a598 -m 282828 -S 83a598 -s 282828
+      ${pkgs.wmenu}/bin/wmenu-run -bif 'JetBrainsMonoNF 12' -p '菜单' -N 282828 -n fe8019 -M 83a598 -m 282828 -S 83a598 -s 282828
     '')
     (import ../scripts/start.nix { inherit pkgs; })
     swaybg # wallpaper setter
