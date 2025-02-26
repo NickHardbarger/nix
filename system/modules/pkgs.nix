6dd3328ac56bf38c06b6553c93abce2c1bd6cc23
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-26 07:47:16 -0500

    tweaked script

--- system/modules/pkgs.nix
+++ system/modules/pkgs.nix
@@ -5,10 +5,9 @@
 {
   environment.systemPackages = with pkgs; [
     (pkgs.writeShellScriptBin "run-wmenu" ''
-      ${pkgs.wmenu}/bin/wmenu-run -if 'JetBrainsMonoNF 12' -l 2 -p '[Wmenu]' -N 282828 -n ebdbb2 -M d65d0e -m 282828 -S d65d0e -s 282828
+      ${pkgs.wmenu}/bin/wmenu-run -if 'JetBrainsMonoNF 12' -p '菜单' -N 282828 -n ebdbb2 -M d65d0e -m 282828 -S d65d0e -s 282828
     '')
     (import ../scripts/start.nix { inherit pkgs; })
-    #wbg # wallpaper setter #!!build failure
     swaybg # wallpaper setter
     grim # screenshots
     slurp # screen select
