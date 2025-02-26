ac54b7bc376eba5a55f18d8c9fb499cb1eab3d8b
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-26 03:58:02 -0500

    added cjk font

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -6,6 +6,7 @@
   fonts.packages = with pkgs; [
     #(nerdfonts.override { fonts = [ "JetBrainsMono" ]; }) # old
     nerd-fonts.jetbrains-mono # new
+    noto-fonts-cjk-sans # cjk support
   ];
   console.font = null; # TODO: figure out console fonts
 }
