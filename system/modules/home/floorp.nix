c69ce4f89e453c1efa1fded3029ce9ea5ad27b88
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-28 17:47:27 -0500

    added bookmarks

--- system/modules/home/floorp.nix
+++ system/modules/home/floorp.nix
@@ -16,6 +16,54 @@
               name = "YouTube";
               url = "https://www.youtube.com/";
             }
+            {
+              name = "GitHub";
+              url = "https://github.com/dashboard";
+            }
+            {
+              name = "nixpkgs";
+              url = "https://search.nixos.org/packages?";
+            }
+            {
+              name = "options";
+              url = "https://search.nixos.org/options?";
+            }
+            {
+              name = "home-manager";
+              url = "https://home-manager-options.extranix.com/?query=&release=master";
+            }
+            {
+              name = "melpa";
+              url = "https://melpa.org/#/";
+            }
+            {
+              name = "GPA Calc";
+              url = "https://gpacalculator.net/college-gpa-calculator/";
+            }
+            {
+              name = "Indeed";
+              url = "https://www.indeed.com/?from=notifcenter_webapp_zero_notifications_page";
+            }
+            {
+              name = "gruvbox";
+              url = "https://github.com/morhetz/gruvbox";
+            }
+            {
+              name = "calendar";
+              url = "https://www.cotc.edu/sites/main/files/file-attachments/2024-2025_academic_calendar.pdf?1725366617";
+            }
+            {
+              name = "breadcrumb";
+              url = "https://github.com/joaotavora/breadcrumb";
+            }
+            {
+              name = "scenefx";
+              url = "https://codeberg.org/dwl/dwl-patches/src/branch/main/patches/scenefx";
+            }
+            {
+              name = "barborder";
+              url = "https://codeberg.org/dwl/dwl-patches/src/branch/main/patches/barborder/barborder.patch";
+            }
           ];
         }
       ];
