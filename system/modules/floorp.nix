17bb7206d92a0723ee366c706005bdd23ebe0d0f
NickHardbarger <nickhardbarger13@gmail.com>
2025-02-28 17:09:34 -0500

    added bookmark

--- system/modules/floorp.nix
+++ system/modules/floorp.nix
@@ -7,5 +7,13 @@
 {
   programs.floorp = {
     enable = true;
+    profiles."nickh" = {
+      bookmarks = [
+        {
+          name = "YouTube";
+          url = "https://www.youtube.com/";
+        }
+      ];
+    };
   };
 }
