3a44dbfa2cb1ef058f8463161385885371dc08c7
NickHardbarger <nickhardbarger13@gmail.com>
2025-03-07 20:27:27 -0500

    added padding

--- system/modules/foot.nix
+++ system/modules/foot.nix
@@ -10,7 +10,10 @@
     enable = true;
     theme = "gruvbox-dark";
     settings = {
-      main.font = "JetBrainsMonoNF:size=12";
+      main = {
+        font = "JetBrainsMonoNF:size=12";
+        pad = "5x5";
+      };
       colors.alpha = 0.9;
       scrollback.lines = 100000;
     };
