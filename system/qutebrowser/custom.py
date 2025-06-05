7cfc312bb0e4edb2459f1425bc08db62edc01922
NickHardbarger <nickhardbarger13@gmail.com>
2025-06-05 05:13:17 -0400

    qutebrowser: added adblock lists

--- system/qutebrowser/custom.py
+++ system/qutebrowser/custom.py
@@ -539,7 +539,25 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 c.content.autoplay = False
 
 # Adblock
-c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt', 'https://easylist.to/easylist/easyprivacy.txt']
+c.content.blocking.adblock.lists = ["https://easylist.to/easylist/easylist.txt",
+                                    "https://easylist.to/easylist/easyprivacy.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2022.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2023.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2024.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-cookies.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances-others.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/quick-fixes.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
+                                    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
 c.content.blocking.enabled = True
 c.content.blocking.hosts.block_subdomains = True
 c.content.blocking.hosts.lists = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts']
