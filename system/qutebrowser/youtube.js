d5ed1ddf3c52d0d359afef7cc0d0033eedc9d3cc
NickHardbarger <nickhardbarger13@gmail.com>
2025-06-05 09:20:24 -0400

    qutebrowser: alternative youtube adblock script

--- system/qutebrowser/youtube.js
+++ system/qutebrowser/youtube.js
@@ -1,19 +1,18 @@
 // ==UserScript==
-// @name         Auto Skip YouTube Ads
-// @version      1.1.0
-// @description  Speed up and skip YouTube ads automatically
-// @author       jso8910 and others
+// @name         Auto Skip YouTube Ads 
+// @version      1.0.0
+// @description  Speed up and skip YouTube ads automatically 
+// @author       jso8910
 // @match        *://*.youtube.com/*
+// @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
-
-
-document.addEventListener('load', () => {
-	const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern');
+setInterval(() => {
+	const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
 	if (btn) {
-		btn.click();
+		btn.click()
 	}
 	const ad = [...document.querySelectorAll('.ad-showing')][0];
 	if (ad) {
-		document.querySelector('video').currentTime = 9999999999;
+		document.querySelector('video').playbackRate = 10;
 	}
-}, true);
+}, 50)
