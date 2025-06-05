a9995e5ccdc1a7368b99d2c4377434a0ed359660
NickHardbarger <nickhardbarger13@gmail.com>
2025-06-05 03:16:21 -0400

    configured qutebrowser font
    tweak
    
    yess

--- system/qutebrowser/custom.py
+++ system/qutebrowser/custom.py
@@ -1193,120 +1193,37 @@ c.content.user_stylesheets = ["~/.config/qutebrowser/styles.css"]
 ## Type: ShellCommand
 # c.fileselect.single_file.command = ['xterm', '-e', 'ranger', '--choosefile={}']
 
-## Font used in the completion categories.
-## Type: Font
-# c.fonts.completion.category = 'bold default_size default_family'
+# Fonts
+c.fonts.default_family = ["JetBrainsMonoNF"]
+c.fonts.default_size = '12pt'
 
-## Font used in the completion widget.
-## Type: Font
-# c.fonts.completion.entry = 'default_size default_family'
+c.fonts.completion.category = 'bold default_size default_family'
+c.fonts.completion.entry = 'default_size default_family'
+c.fonts.contextmenu = 'default_size default_family'
+c.fonts.debug_console = 'default_size default_family'
+c.fonts.downloads = 'default_size default_family'
+c.fonts.hints = 'bold default_size default_family'
+c.fonts.keyhint = 'default_size default_family'
+c.fonts.messages.error = 'default_size default_family'
+c.fonts.messages.info = 'default_size default_family'
+c.fonts.messages.warning = 'default_size default_family'
+c.fonts.prompts = 'default_size sans-serif'
+c.fonts.statusbar = 'default_size default_family'
+c.fonts.tabs.selected = 'default_size default_family'
+c.fonts.tabs.unselected = 'default_size default_family'
+c.fonts.tooltip = 'default_size default_family'
 
-## Font used for the context menu. If set to null, the Qt default is
-## used.
-## Type: Font
-# c.fonts.contextmenu = None
+c.fonts.web.family.cursive = 'monospace'
+c.fonts.web.family.fantasy = 'monospace'
+c.fonts.web.family.fixed = 'monospace'
+c.fonts.web.family.sans_serif = 'monospace'
+c.fonts.web.family.serif = 'monospace'
+c.fonts.web.family.standard = 'monospace'
 
-## Font used for the debugging console.
-## Type: Font
-# c.fonts.debug_console = 'default_size default_family'
-
-## Default font families to use. Whenever "default_family" is used in a
-## font setting, it's replaced with the fonts listed here. If set to an
-## empty value, a system-specific monospace default is used.
-## Type: List of Font, or Font
-# c.fonts.default_family = []
-
-## Default font size to use. Whenever "default_size" is used in a font
-## setting, it's replaced with the size listed here. Valid values are
-## either a float value with a "pt" suffix, or an integer value with a
-## "px" suffix.
-## Type: String
-# c.fonts.default_size = '10pt'
-
-## Font used for the downloadbar.
-## Type: Font
-# c.fonts.downloads = 'default_size default_family'
-
-## Font used for the hints.
-## Type: Font
-# c.fonts.hints = 'bold default_size default_family'
-
-## Font used in the keyhint widget.
-## Type: Font
-# c.fonts.keyhint = 'default_size default_family'
-
-## Font used for error messages.
-## Type: Font
-# c.fonts.messages.error = 'default_size default_family'
-
-## Font used for info messages.
-## Type: Font
-# c.fonts.messages.info = 'default_size default_family'
-
-## Font used for warning messages.
-## Type: Font
-# c.fonts.messages.warning = 'default_size default_family'
-
-## Font used for prompts.
-## Type: Font
-# c.fonts.prompts = 'default_size sans-serif'
-
-## Font used in the statusbar.
-## Type: Font
-# c.fonts.statusbar = 'default_size default_family'
-
-## Font used for selected tabs.
-## Type: Font
-# c.fonts.tabs.selected = 'default_size default_family'
-
-## Font used for unselected tabs.
-## Type: Font
-# c.fonts.tabs.unselected = 'default_size default_family'
-
-## Font used for tooltips. If set to null, the Qt default is used.
-## Type: Font
-# c.fonts.tooltip = None
-
-## Font family for cursive fonts.
-## Type: FontFamily
-# c.fonts.web.family.cursive = ''
-
-## Font family for fantasy fonts.
-## Type: FontFamily
-# c.fonts.web.family.fantasy = ''
-
-## Font family for fixed fonts.
-## Type: FontFamily
-# c.fonts.web.family.fixed = ''
-
-## Font family for sans-serif fonts.
-## Type: FontFamily
-# c.fonts.web.family.sans_serif = ''
-
-## Font family for serif fonts.
-## Type: FontFamily
-# c.fonts.web.family.serif = ''
-
-## Font family for standard fonts.
-## Type: FontFamily
-# c.fonts.web.family.standard = ''
-
-## Default font size (in pixels) for regular text.
-## Type: Int
-# c.fonts.web.size.default = 16
-
-## Default font size (in pixels) for fixed-pitch text.
-## Type: Int
-# c.fonts.web.size.default_fixed = 13
-
-## Hard minimum font size (in pixels).
-## Type: Int
-# c.fonts.web.size.minimum = 0
-
-## Minimum logical font size (in pixels) that is applied when zooming
-## out.
-## Type: Int
-# c.fonts.web.size.minimum_logical = 6
+c.fonts.web.size.default = 16
+c.fonts.web.size.default_fixed = 13
+c.fonts.web.size.minimum = 0
+c.fonts.web.size.minimum_logical = 6
 
 ## When a hint can be automatically followed without pressing Enter.
 ## Type: String
