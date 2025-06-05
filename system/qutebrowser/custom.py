6d48c904acf8ce74071898fa1e60267c13e5da94
NickHardbarger <nickhardbarger13@gmail.com>
2025-06-05 00:05:22 -0400

    config tweaks
    err

--- system/qutebrowser/custom.py
+++ system/qutebrowser/custom.py
@@ -1,5 +1,6 @@
-c.tabs.background = True
-c.tabs.show = 'never'
+# c.tabs.background = True
+# c.tabs.show = 'never'
 # c.statusbar.hide = False # gives error
-c.url.searchengines["g"] = "https://www.google.com.ar/search?q={}"
-c.url.searchengines["DEFAULT"] = "https://www.google.com.ar/search?q={}"
+c.url.searchengines = {
+    'DEFAULT': 'https://duckduckgo.com/?q={}',
+}
