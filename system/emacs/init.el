17eff598432c886b492e053c39d7f286b9663be0
Nick <nickhardbarger13@gmail.com>
2025-07-10 22:36:06 -0400

    emacs: configured org-mode header sizes

--- system/emacs/init.el
+++ system/emacs/init.el
@@ -59,7 +59,14 @@
    '((emacs-lisp . t)
      (shell . t)))
   (add-to-list 'org-agenda-files "~/notes/agenda.org")
-  (setq org-clock-sound "~/.emacs.d/ding.wav")) ; https://freesound.org/people/.Andre_Onate/sounds/484665/
+  (setq org-clock-sound "~/.emacs.d/ding.wav") ; https://freesound.org/people/.Andre_Onate/sounds/484665/
+  
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+   '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+   '(org-level-5 ((t (:inherit outline-5 :height 1.1))))))
 
 (use-package org-drill
   :after org
