942cd5904b807ec8aa2fbd0d81b4c4cfecf123e1
Nick <nickhardbarger13@gmail.com>
2025-07-26 22:39:30 -0400

    emacs: additional icomplete configuration
    sc

--- system/emacs/init-completions.el
+++ system/emacs/init-completions.el
@@ -1,11 +1,29 @@
 (use-package icomplete
   :ensure nil
   :custom
+  (tab-always-indent 'complete)
   (icomplete-delay-completions-threshold 0)
   (icomplete-max-delay-chars 0)
   (icomplete-compute-delay 0)
   (icomplete-show-matches-on-no-input t)
+  ;; icomplete-in-buffer gives corfu-like functionality
+  ;; However, corfu has the following features that I like:
+  ;; -allows scrolling
+  ;; -nerd icons
+  ;; -really nice info popup box (eldoc?)
+  ;; -opens automatically whenever completions are available
+  
+  ;; icomplete-in-buffer also has the following problems:
+  ;; -marginalia makes it look bad, need to figure out how to disable it
+  ;;   -maybe there's a hook I could use? or maybe advice-add?
+  ;; -the completions always appear at far left of screen, instead of at point
+  ;;   -simple patch to fix this on Rahul Juliato's blog, maybe it gets upstreamed?
+  ;; (icomplete-in-buffer t)
+  (icomplete-prospects-height 10)
+  (icomplete-separator " . ")
   :config
+  (advice-add 'completion-at-point
+              :after #'minibuffer-hide-completions)
   (icomplete-vertical-mode 1))
 
 (use-package savehist
