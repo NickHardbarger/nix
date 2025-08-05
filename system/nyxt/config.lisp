582e25f2d98efc616a379d10a8636bb6a28bdd43
Nick <nickhardbarger13@gmail.com>
2025-08-04 22:08:50 -0400

    nyxt: adblocker

--- system/nyxt/config.lisp
+++ system/nyxt/config.lisp
@@ -1,3 +1,7 @@
 (define-configuration buffer
     ((default-modes
 	 (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+(define-configuration web-buffer
+    ((default-modes
+	 (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
