23f880e8c6f3620c6d0bffb64c87ec0846b40944
Nick <nickhardbarger13@gmail.com>
2025-08-21 05:17:18 -0400

    emacs: removed diff-hl

--- system/emacs/init-git.el
+++ system/emacs/init-git.el
@@ -9,13 +9,3 @@
   :after magit
   :ensure t
   :config (magit-todos-mode 1))
-
-(use-package diff-hl
-  :after magit
-  :ensure t
-  :config
-  (diff-hl-margin-mode)
-  (diff-hl-dired-mode)
-  (diff-hl-flydiff-mode)
-  (global-diff-hl-mode)
-  :hook (magit-post-refresh-hook . diff-hl-magit-post-refresh))
